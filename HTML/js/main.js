07f2bfdd980d675c6e453ba88abbf10a98b0f5bb
Allwyn Bat Thomas <allwynt700@gmail.com>
2024-06-24 12:07:39 +0530

    first commit

---
+++ HTML/js/main.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(e) { 
+	
+	(function($) {
+          'use strict';
+          // call our plugin
+          var Nav = new hcOffcanvasNav('#main-nav #menu-main-menu', {
+            disableAt: false,
+            customToggle: '.toggle',
+            levelSpacing: 50,
+            //navTitle: 'All Categories',
+            levelTitles: true,
+            levelTitleAsBack: true,
+            pushContent: '#container-box',
+            labelClose: false
+          });
+
+    })(jQuery);
+	/*-------------*/
+  
+	
+});
+
+
